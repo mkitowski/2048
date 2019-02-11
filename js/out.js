3a4fafe459534e4dbee00d5f3df46b22cb3b0594
mkitowski <maciej.kitowski@gmail.com>
2019-02-11 22:00:54 +0100

    update

---
+++ js/out.js
@@ -0,0 +1,101 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./js/app.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./js/app.js":
+/*!*******************!*\
+  !*** ./js/app.js ***!
+  \*******************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\ndocument.addEventListener(\"DOMContentLoaded\", function () {\n\n    var boxes = document.querySelectorAll('.box');\n    var button = document.querySelector('#start');\n    var scoretext = document.querySelector('.score');\n    var b1 = ['', '', '', '', '', '', '', '', ''];\n    var score = 0;\n\n    function restart() {\n        var result = 0;\n        result = Math.floor(Math.random() * 9);\n        boxes[result].innerHTML = 2;\n        b1[result] = 2;\n        boxes.forEach(function (e, i) {\n            if (i !== result) {\n                boxes[i].innerHTML = '';\n                b1[i] = '';\n                changeBkgColor(i);\n            }\n        });\n        score = 0;\n        changeBkgColor(result);\n        addScore();\n    }\n\n    button.addEventListener('click', function () {\n        restart();\n        this.innerHTML = 'Restart';\n    });\n\n    function addScore() {\n        scoretext.innerHTML = score;\n    }\n\n    function generateNew() {\n        var result;\n\n        for (var i = 0; i < 18; i++) {\n            result = Math.floor(Math.random() * 9);\n            if (b1[result] === \"\") {\n                i = 18;\n                b1[result] = 2;\n                boxes[result].innerHTML = 2;\n                changeBkgColor(result);\n            }\n        }\n\n        addScore();\n    }\n\n    function changeBkgColor(i) {\n        if (b1[i] < 10) {\n            boxes[i].style.backgroundColor = '#' + b1[i] + '0' + b1[i] + 'F00';\n            boxes[i].style.color = 'aqua';\n        } else if (b1[i] < 99 && b1[i] > 10) {\n            boxes[i].style.backgroundColor = '#' + b1[i] + 'FF' + b1[i];\n            boxes[i].style.color = '#3C3F41';\n        } else if (b1[i] < 999 && b1[i] > 100) {\n            boxes[i].style.backgroundColor = '#' + b1[i] + b1[i];\n            boxes[i].style.color = 'aqua';\n        } else if (b1[i] === '') {\n            boxes[i].style.backgroundColor = '#00FFFF';\n        }\n    }\n\n    function movedown() {\n        var moved = false;\n\n        for (var i = 3; i < 6; i++) {\n\n            if (b1[i + 3] === '' && b1[i] !== '') {\n                b1[i + 3] = b1[i];\n                b1[i] = '';\n                boxes[i + 3].innerHTML = b1[i + 3];\n                boxes[i].innerHTML = '';\n                moved = true;\n                changeBkgColor(i + 3);\n                changeBkgColor(i);\n            } else if (b1[i] === b1[i + 3] && b1[i] !== '') {\n                b1[i + 3] = Number(b1[i + 3]) * 2;\n                b1[i] = '';\n                boxes[i + 3].innerHTML = b1[i + 3];\n                boxes[i].innerHTML = b1[i];\n                moved = true;\n                score += b1[i + 3];\n                changeBkgColor(i + 3);\n                changeBkgColor(i);\n            }\n        }\n\n        for (i = 0; i < 3; i++) {\n\n            if (b1[i + 3] === '' && b1[i + 6] === '' && b1[i] !== '') {\n                b1[i + 6] = b1[i];\n                b1[i] = '';\n                boxes[i + 6].innerHTML = b1[i + 6];\n                boxes[i].innerHTML = b1[i];\n                moved = true;\n                changeBkgColor(i + 6);\n                changeBkgColor(i);\n            } else if (b1[i + 3] === '' && b1[i + 6] === b1[i] && b1[i] !== '') {\n                b1[i + 6] = Number(b1[i + 6]) * 2;\n                b1[i] = '';\n                boxes[i + 6].innerHTML = b1[i + 6];\n                boxes[i].innerHTML = b1[i];\n                score += b1[i + 6];\n                moved = true;\n                changeBkgColor(i + 6);\n                changeBkgColor(i);\n            } else if (b1[i + 3] === '' && b1[i] !== '') {\n                b1[i + 3] = b1[i];\n                b1[i] = '';\n                boxes[i + 3].innerHTML = b1[i + 3];\n                boxes[i].innerHTML = b1[i];\n                moved = true;\n                changeBkgColor(i + 3);\n                changeBkgColor(i);\n            } else if (b1[i + 3] === b1[i] && b1[i] !== '') {\n                b1[i + 3] = Number(b1[i + 3]) * 2;\n                b1[i] = '';\n                boxes[i + 3].innerHTML = b1[i + 3];\n                boxes[i].innerHTML = b1[i];\n                moved = true;\n                score += b1[i + 3];\n                changeBkgColor(i + 3);\n                changeBkgColor(i);\n            }\n        }\n\n        if (moved) {\n            generateNew();\n        }\n    }\n\n    function moveup() {\n        var moved = false;\n\n        for (var i = 3; i < 6; i++) {\n\n            if (b1[i - 3] === '' && b1[i] !== '') {\n                b1[i - 3] = b1[i];\n                b1[i] = '';\n                boxes[i - 3].innerHTML = b1[i - 3];\n                boxes[i].innerHTML = '';\n                moved = true;\n                changeBkgColor(i - 3);\n                changeBkgColor(i);\n            } else if (b1[i] === b1[i - 3] && b1[i] !== '') {\n                b1[i - 3] = Number(b1[i - 3]) * 2;\n                b1[i] = '';\n                boxes[i - 3].innerHTML = b1[i - 3];\n                boxes[i].innerHTML = b1[i];\n                moved = true;\n                score += b1[i - 3];\n                changeBkgColor(i - 3);\n                changeBkgColor(i);\n            }\n        }\n\n        for (i = 6; i < 9; i++) {\n\n            if (b1[i - 3] === '' && b1[i - 6] === '' && b1[i] !== '') {\n                b1[i - 6] = b1[i];\n                b1[i] = '';\n                boxes[i - 6].innerHTML = b1[i - 6];\n                boxes[i].innerHTML = b1[i];\n                moved = true;\n                changeBkgColor(i - 6);\n                changeBkgColor(i);\n            } else if (b1[i - 3] === '' && b1[i - 6] === b1[i] && b1[i] !== '') {\n                b1[i - 6] = Number(b1[i - 6]) * 2;\n                b1[i] = '';\n                boxes[i - 6].innerHTML = b1[i - 6];\n                boxes[i].innerHTML = b1[i];\n                score += b1[i - 6];\n                moved = true;\n                changeBkgColor(i - 6);\n                changeBkgColor(i);\n            } else if (b1[i - 3] === '' && b1[i] !== '') {\n                b1[i - 3] = b1[i];\n                b1[i] = '';\n                boxes[i - 3].innerHTML = b1[i - 3];\n                boxes[i].innerHTML = b1[i];\n                moved = true;\n                changeBkgColor(i - 3);\n                changeBkgColor(i);\n            } else if (b1[i - 3] === b1[i] && b1[i] !== '') {\n                b1[i - 3] = Number(b1[i - 3]) * 2;\n                b1[i] = '';\n                boxes[i - 3].innerHTML = b1[i - 3];\n                boxes[i].innerHTML = b1[i];\n                moved = true;\n                score += b1[i - 3];\n                changeBkgColor(i - 3);\n                changeBkgColor(i);\n            }\n        }\n\n        if (moved) {\n            generateNew();\n        }\n    }\n\n    function moveright() {\n        var moved = false;\n\n        for (var i = 1; i < 8; i += 3) {\n            if (b1[i + 1] === '' && b1[i] !== '') {\n                b1[i + 1] = b1[i];\n                b1[i] = '';\n                boxes[i + 1].innerHTML = b1[i + 1];\n                boxes[i].innerHTML = '';\n                moved = true;\n                changeBkgColor(i + 1);\n                changeBkgColor(i);\n            } else if (b1[i] === b1[i + 1] && b1[i] !== '') {\n                b1[i + 1] = Number(b1[i + 1]) * 2;\n                b1[i] = '';\n                boxes[i + 1].innerHTML = b1[i + 1];\n                boxes[i].innerHTML = b1[i];\n                moved = true;\n                score += b1[i + 1];\n                changeBkgColor(i + 1);\n                changeBkgColor(i);\n            }\n        }\n\n        for (i = 0; i < 7; i += 3) {\n            if (b1[i + 1] === '' && b1[i + 2] === '' && b1[i] !== '') {\n                b1[i + 2] = b1[i];\n                b1[i] = '';\n                boxes[i + 2].innerHTML = b1[i + 2];\n                boxes[i].innerHTML = '';\n                moved = true;\n                changeBkgColor(i + 2);\n                changeBkgColor(i);\n            } else if (b1[i + 1] === '' && b1[i + 2] === b1[i] && b1[i] !== '') {\n                b1[i + 2] = Number(b1[i + 2]) * 2;\n                b1[i] = '';\n                boxes[i + 2].innerHTML = b1[i + 2];\n                boxes[i].innerHTML = b1[i];\n                moved = true;\n                score += b1[i + 2];\n                changeBkgColor(i + 2);\n                changeBkgColor(i);\n            } else if (b1[i + 1] === '' && b1[i] !== '') {\n                b1[i + 1] = b1[i];\n                b1[i] = '';\n                boxes[i + 1].innerHTML = b1[i + 1];\n                boxes[i].innerHTML = b1[i];\n                moved = true;\n                changeBkgColor(i + 1);\n                changeBkgColor(i);\n            } else if (b1[i + 1] === b1[i] && b1[i] !== '') {\n                b1[i + 1] = Number(b1[i + 1]) * 2;\n                b1[i] = '';\n                boxes[i + 1].innerHTML = b1[i + 1];\n                boxes[i].innerHTML = b1[i];\n                moved = true;\n                score += b1[i + 1];\n                changeBkgColor(i + 1);\n                changeBkgColor(i);\n            }\n        }\n        if (moved) {\n            generateNew();\n        }\n    }\n\n    function moveleft() {\n\n        var moved = false;\n\n        for (var i = 7; i > 0; i -= 3) {\n            if (b1[i - 1] === '' && b1[i] !== '') {\n                b1[i - 1] = b1[i];\n                b1[i] = '';\n                boxes[i - 1].innerHTML = b1[i - 1];\n                boxes[i].innerHTML = '';\n                moved = true;\n                changeBkgColor(i - 1);\n                changeBkgColor(i);\n            } else if (b1[i] === b1[i - 1] && b1[i] !== '') {\n                b1[i - 1] = Number(b1[i - 1]) * 2;\n                b1[i] = '';\n                boxes[i - 1].innerHTML = b1[i - 1];\n                boxes[i].innerHTML = b1[i];\n                moved = true;\n                score += b1[i - 1];\n                changeBkgColor(i - 1);\n                changeBkgColor(i);\n            }\n        }\n\n        for (i = 8; i > 1; i -= 3) {\n            if (b1[i - 1] === '' && b1[i - 2] === '' && b1[i] !== '') {\n                b1[i - 2] = b1[i];\n                b1[i] = '';\n                boxes[i - 2].innerHTML = b1[i - 2];\n                boxes[i].innerHTML = '';\n                moved = true;\n                changeBkgColor(i - 2);\n                changeBkgColor(i);\n            } else if (b1[i - 1] === '' && b1[i - 2] === b1[i] && b1[i] !== '') {\n                b1[i - 2] = Number(b1[i - 2]) * 2;\n                b1[i] = '';\n                boxes[i - 2].innerHTML = b1[i - 2];\n                boxes[i].innerHTML = b1[i];\n                moved = true;\n                score += b1[i - 2];\n                changeBkgColor(i - 2);\n                changeBkgColor(i);\n            } else if (b1[i - 1] === '' && b1[i] !== '') {\n                b1[i - 1] = b1[i];\n                b1[i] = '';\n                boxes[i - 1].innerHTML = b1[i - 1];\n                boxes[i].innerHTML = b1[i];\n                moved = true;\n                changeBkgColor(i - 1);\n                changeBkgColor(i);\n            } else if (b1[i - 1] === b1[i] && b1[i] !== '') {\n                b1[i - 1] = Number(b1[i - 1]) * 2;\n                b1[i] = '';\n                boxes[i - 1].innerHTML = b1[i - 1];\n                boxes[i].innerHTML = b1[i];\n                moved = true;\n                score += b1[i - 1];\n                changeBkgColor(i - 1);\n                changeBkgColor(i);\n            }\n        }\n        if (moved) {\n            generateNew();\n        }\n    }\n\n    function moveit(key) {\n        if (key === 83 || key === 40) {\n            //down\n            movedown();\n        } else if (key === 87 || key === 38) {\n            //up\n            moveup();\n        } else if (key === 68 || key === 39) {\n            //right\n            moveright();\n        } else if (key === 65 || key === 37) {\n            //left\n            moveleft();\n        }\n    }\n\n    window.addEventListener('keydown', function (e) {\n\n        moveit(e.keyCode);\n    });\n\n    var pointx, pointy, pointsx, pointsy;\n\n    window.addEventListener('touchstart', function (e) {\n\n        var touchobj = e.changedTouches[0];\n\n        pointx = touchobj.pageX;\n        pointy = touchobj.pageY;\n    }, false);\n\n    window.addEventListener('touchmove', function (ev) {\n        if (window) {\n            ev.preventDefault();\n            ev.stopImmediatePropagation();\n        }\n    }, { passive: false });\n\n    window.addEventListener('touchend', function (e) {\n        var touchobj = e.changedTouches[0];\n\n        pointsx = touchobj.pageX;\n        pointsy = touchobj.pageY;\n        var axlex = pointsx - pointx;\n        var axley = pointsy - pointy;\n\n        if (Math.abs(axlex) + Math.abs(axley) > 100) {\n\n            if (axlex >= 0 && axley > 0 && axley > axlex || axlex <= 0 && axley > 0 && axley > -axlex) {\n                movedown();\n            } else if (axlex >= 0 && axley < 0 && -axley > axlex || axlex <= 0 && axley < 0 && axley < axlex) {\n                moveup();\n            } else if (axlex > 0 && axley >= 0 && axlex > axley || axlex > 0 && axley <= 0 && -axley < axlex) {\n                moveright();\n            } else {\n                moveleft();\n            }\n        }\n    }, false);\n});\n\n//# sourceURL=webpack:///./js/app.js?");
+
+/***/ })
+
+/******/ });
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+
+module.exports = {
+    entry: "./js/app.js",
+    output: {
+        filename: "out.js",
+        path: path.resolve(__dirname, 'js')
+    },
+    mode: 'development',
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    }
+}
