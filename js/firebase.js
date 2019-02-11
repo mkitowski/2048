f4c1fb8b75656ecabf5b6b397d45761f7f9d0fb5
Maciej Kitowski <Kitowskim@DelapWaller.net>
2019-02-11 14:57:53 +0000

    sign up lauput made

--- js/firebase.js
+++ js/firebase.js
@@ -19,3 +19,29 @@ docRef.get().then((doc) =>{
    }
   
 });
+
+
+
+const signin = $('#sign-up');
+const message = $('.message');
+const close = $('#close');
+let email, password;
+
+close.on('click', () => message.removeClass('active'));
+
+signin.click ( () => {
+    message.addClass('active');
+    let h2 = $('<h2>').text('Register');
+    let login = $('<div>',{class: 'login'} );
+    let label1 = $('<label>').text('Your email address');
+    let input1 = $('<input>').attr('type','email').attr('placeholder','your@email.2048');
+    let label2 = $('<label>').text('Your password');
+    let input2 = $('<input>').attr('type', 'password').attr('placeholder', 'your new password');
+    let label3 = $('<label>').text('Re-type your password');
+    let input3 = $('<input>').attr('type', 'password').attr('placeholder', 'your new password again');
+    let btn = $('<button>').text('Create account');
+    console.log(login);
+    login.append(label1).append(input1).append(label2).append(input2).append(label3).append(input3).append(btn);
+
+    message.append(h2).append(login);
+});
