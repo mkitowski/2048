f220c208fb2c0ef9205fa6e7b6af32f77a429273
mkitowski <maciej.kitowski@gmail.com>
2019-02-12 23:20:27 +0100

    login updated

--- js/firebase.js
+++ js/firebase.js
@@ -22,173 +22,201 @@ docRef.get().then((doc) =>{
 
 
 
-const signup = $('#sign-up');
-const signin = $('#sign-in');
+
 const message = $('.message');
 const close = $('#close');
-const navbar = $('.users');
 let email, password;
 
-close.on('click', () => { //ZAMKNIECIE OKNA
-    message.removeClass('active');
-    $('.login').remove();
-});
-
-signup.click ( () => { //OTWARCIE OKNA REJESTRACJI
-    message.addClass('active');
-    const h2 = $('<h2>').text('Register');
-    const login = $('<form>',{class: 'login'} );
-    const label = $('<label>').text('Username:');
-    const input = $('<input>', {class: 'name'}).attr('type','text').attr('placeholder','Your name');
-    const label1 = $('<label>', {class: 'l-email'}).text('Your email address:');
-    const input1 = $('<input>', {class: 'email'}).attr('type','email').attr('placeholder','your@email.2048');
-    const label2 = $('<label>', {class: 'l-pass'}).text('Your password:');
-    const input2 = $('<input>', {class: 'password1'}).attr('type', 'password').attr('placeholder', 'your new password');
-    const label3 = $('<label>').text('Re-type your password:');
-    const input3 = $('<input>', {class: 'password2'}).attr('type', 'password').attr('placeholder', 'your new password again');
-    const btn = $('<button>',{class: 'register'}).text('Create account');
-   
-    login.append(h2)
-        .append(label).append(input)
-        .append(label1).append(input1)
-        .append(label2).append(input2)
-        .append(label3).append(input3)
-        .append(btn);
-    message.append(login);
-});
-
-message.on('click', '.register', e => { //PROCES REJESTRACJI
-    e.preventDefault();
-    const email = document.querySelector('.email').value;
-    const pass1 = $('.password1').val();
-    const pass2 = $('.password2').val();
-    const name = $('.name').val();
-    const label1 = $('.l-email');
-    const label2 = $('.l-pass');
-
-    let acceptable = true;
-   
-    if (email.indexOf('@')<0) {
-        acceptable = false;
-        label1.text('Podaj poprawny email');
-    }else {
-        label1.text('email ok');
-    }
-    if (email.indexOf('.')<0) {
-        acceptable = false;
-        label1.text('Podaj poprawny email');
-    } else {
-        label1.text('email ok');
-    }
-    if (pass1 !== pass2) {
-        acceptable = false;
-        label2.text('Hasła nie zgadzają się')
-    } else {
-        label2.text('hasło ok');
-    }
-    if (pass1.length < 5) {
-        acceptable = false;
-        label2.text('Hasło powinno mieć przynajmniej 6 znaków')
-    } else {
-        label2.text('hasło ok');
-    }
-
-    if (acceptable) {
-       
-        let user = null;
-        firebase.auth().createUserWithEmailAndPassword(email, pass1)
-        .then(() => {
-            user = firebase.auth().currentUser;
-            signin.text(name).attr('id', "profile");
-            signup.text('Sign-off').attr('id', 'sign-off');
-            console.log(name);
-            console.log(signin.text());
-        })
-        .then(() => {
-            user.updateProfile({
-                displayName: name
-            });
-            message.removeClass('active');
-            $('.login').remove();
-        })
-        .catch( error => {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.log(errorCode);
-            console.log(errorMessage);
-        });
-
-       
-        
-   
-        
-    }
-});
-
-signin.click ( () => { //OTWARCIE OKNA LOGOWANIA
-    message.addClass('active');
-    const h2 = $('<h2>').text('Sign in');
-    const login = $('<form>',{class: 'login'} );
-    const input = $('<input>', {class: 'name'}).attr('type','text').attr('placeholder','Your name');
-    const label1 = $('<label>', {class: 'l-email'}).text('Your email address');
-    const input1 = $('<input>', {class: 'email'}).attr('type','email').attr('placeholder','your@email.2048');
-    const label2 = $('<label>', {class: 'l-pass'}).text('Your password');
-    const input2 = $('<input>', {class: 'password'}).attr('type', 'password').attr('placeholder', 'your new password');
-    const btn = $('<button>',{class: 'log-in'}).text('Sign-in');
-    console.log(login);
-    login.append(h2)
-        .append(label1).append(input1)
-        .append(label2).append(input2)
-        .append(btn);
-    message.append(login);
-});
-
-message.on('click', '.log-in', e => { //PROCES LOGOWANIA
-    e.preventDefault();
-    const email = document.querySelector('.email').value;
-    const pass = $('.password').val();
-
-    firebase.auth().signInWithEmailAndPassword(email, pass)
-    .catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        window.alert('errorCode + errorMessage');
-
-    }).then(() => {
+const closeWindow = () => {
+    close.on('click', () => { //ZAMKNIECIE OKNA
         message.removeClass('active');
         $('.login').remove();
     });
+}
 
-});
+const registration = () =>{
 
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-        // User is signed in.
-        var dispName = user.displayName;
-        var email = user.email;
-        var emailVerified = user.emailVerified;
-        signup.text('Sign-off').attr('id','sign-off');
-        signin.text(dispName).attr('id',"profile");
-    } else {
-        // User is signed out.
-        // ...
-        
-    }
-});
+    let signup = $('.sign-up');
+    let signin = $('.sign-in');
+    signup.click ( () => { //OTWARCIE OKNA REJESTRACJI
+        message.addClass('active');
+        const h2 = $('<h2>').text('Register');
+        const login = $('<form>',{class: 'login'} );
+        const label = $('<label>').text('Username:');
+        const input = $('<input>', {class: 'name'}).attr('type','text').attr('placeholder','Your name');
+        const label1 = $('<label>', {class: 'l-email'}).text('Your email address:');
+        const input1 = $('<input>', {class: 'email'}).attr('type','email').attr('placeholder','your@email.2048');
+        const label2 = $('<label>', {class: 'l-pass'}).text('Your password:');
+        const input2 = $('<input>', {class: 'password1'}).attr('type', 'password').attr('placeholder', 'your new password');
+        const label3 = $('<label>').text('Re-type your password:');
+        const input3 = $('<input>', {class: 'password2'}).attr('type', 'password').attr('placeholder', 'your new password again');
+        const btn = $('<button>',{class: 'register'}).text('Create account');
 
-navbar.on('click','#sign-off', e => {
-    firebase.auth().signOut().then( () => {
-        window.alert('Do zobaczenia!');
-        const logout = $('#sign-off');
-        const prof = $('#profile');
-        setTimeout ( () => {
-            logout.text('Sign-up').attr('id', 'sign-up');
-            prof.text('Sign-in').attr('id', "sign-in");
-        }, 300);
-    }, function(error) {
-        window.alert('Coś poszło nie tak');
+        login.append(h2)
+            .append(label).append(input)
+            .append(label1).append(input1)
+            .append(label2).append(input2)
+            .append(label3).append(input3)
+            .append(btn);
+        message.append(login);
+    }); //Registration window
+
+    message.on('click', '.register', e => { //PROCES REJESTRACJI
+        e.preventDefault();
+        const email = document.querySelector('.email').value;
+        const pass1 = $('.password1').val();
+        const pass2 = $('.password2').val();
+        const name = $(".name").val();
+        const label1 = $('.l-email');
+        const label2 = $('.l-pass');
+
+        let acceptable = true;
+
+        if (email.indexOf('@')<0) {
+            acceptable = false;
+            label1.text('Podaj poprawny email');
+        }else {
+            label1.text('email ok');
+        }
+        if (email.indexOf('.')<0) {
+            acceptable = false;
+            label1.text('Podaj poprawny email');
+        } else {
+            label1.text('email ok');
+        }
+        if (pass1 !== pass2) {
+            acceptable = false;
+            label2.text('Hasła nie zgadzają się')
+        } else {
+            label2.text('hasło ok');
+        }
+        if (pass1.length < 5) {
+            acceptable = false;
+            label2.text('Hasło powinno mieć przynajmniej 6 znaków')
+        } else {
+            label2.text('hasło ok');
+        }
+
+        if (acceptable) {
+
+            let user = null;
+            firebase.auth().createUserWithEmailAndPassword(email, pass1)
+                .then(() => {
+                    user = firebase.auth().currentUser;
+
+                }).then(() => {
+                user.updateProfile({
+                    displayName: name
+                });
+                signup.text('Sign-off').removeClass('sign-up').addClass('sign-off');
+                signin.text(name).removeClass('sign-in').addClass('profile');
+                console.log(name);
+                message.removeClass('active');
+                $('.login').remove();
+            })
+                .catch( error => {
+                    // Handle Errors here.
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    console.log(errorCode);
+                    console.log(errorMessage);
+                });
+
+
+
+
+
+        }
+    }); //registration proccess
+}
+
+const log = () => {
+
+    let signin = $('.sign-in');
+    signin.click ( () => { //OTWARCIE OKNA LOGOWANIA
+        message.addClass('active');
+        const h2 = $('<h2>').text('Sign in');
+        const login = $('<form>',{class: 'login'} );
+        const input = $('<input>', {class: 'name'}).attr('type','text').attr('placeholder','Your name');
+        const label1 = $('<label>', {class: 'l-email'}).text('Your email address');
+        const input1 = $('<input>', {class: 'email'}).attr('type','email').attr('placeholder','your@email.2048');
+        const label2 = $('<label>', {class: 'l-pass'}).text('Your password');
+        const input2 = $('<input>', {class: 'password'}).attr('type', 'password').attr('placeholder', 'your new password');
+        const btn = $('<button>',{class: 'log-in'}).text('Sign-in');
+        console.log(login);
+        login.append(h2)
+            .append(label1).append(input1)
+            .append(label2).append(input2)
+            .append(btn);
+        message.append(login);
     });
-})
+
+    message.on('click', '.log-in', e => { //PROCES LOGOWANIA
+        e.preventDefault();
+        const email = document.querySelector('.email').value;
+        const pass = $('.password').val();
+
+        firebase.auth().signInWithEmailAndPassword(email, pass)
+            .catch(function(error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                window.alert('errorCode + errorMessage');
+
+            }).then(() => {
+            message.removeClass('active');
+            $('.login').remove();
+        });
+
+    });
+
+}
+
+const signOff = () => {
+    const navbar = $('.users');
+    navbar.on('click','.sign-off', e => {
+        firebase.auth().signOut().then( () => {
+            window.alert('Do zobaczenia!');
+            const logout = $('.sign-off');
+            const prof = $('.profile');
+            setTimeout ( () => {
+                logout.text('Sign-up').addClass('sign-up').removeClass('sign-off');
+                prof.text('Sign-in').addClass('sign-in').removeClass('profile');
+            }, 300);
+        }, function(error) {
+            window.alert('Coś poszło nie tak');
+        });
+    })
+
+}
+
+registration();
+log();
+closeWindow();
+signOff();
+
+window.onload = ev => {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+            var dispName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            let signup = $('.sign-up');
+            let signin = $('.sign-in');
+
+            signup.text('Sign-off').removeClass('sign-up').addClass('sign-off');
+            signin.text(dispName).removeClass('sign-in').addClass('profile');
+
+
+        } else {
+            // User is signed out.
+            // ...
+
+        }
+    });
+};
+
+
+
 
