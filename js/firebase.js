ed418abf0556ebd97164e304d2fc02e64d553ecc
Maciej Kitowski <Kitowskim@DelapWaller.net>
2019-03-04 11:52:50 +0000

    webpack added

--- js/firebase.js
+++ js/firebase.js
@@ -1,3 +1,6 @@
+
+//classes
+
 var config = {
     apiKey: "AIzaSyDYxBVHNFeaTZgR0o09BvoZkQ0Ugy9zuME",
     authDomain: "project-6559579986434785633.firebaseapp.com",
@@ -8,63 +11,9 @@ var config = {
 };
 firebase.initializeApp(config);
 
+import { Db } from "./database/db";
 
 
-
-//classes
-
-class Db {
-    constructor (path) {
-        this.path = path;
-    }
-
-    connect() {
-        const db = firebase.firestore();
-        const docRef = db.doc(this.path);
-        return docRef;
-    }
-
-    existName(name) {
-        let userexists;
-        return this.connect(this.path).get()
-            .then(querySnapshot => {
-                let users = querySnapshot.data().Users;
-                if (users.indexOf(name) > -1) {
-                    userexists = true;
-                    return userexists;
-                } else {
-                    return false;
-                }
-            })
-            .catch(e => {
-                console.log(e);
-            })
-    }
-
-    addName(name) {
-        this.connect(this.path).update({ Users: firebase.firestore.FieldValue.arrayUnion(name) })
-    }
-
-    getInstructions() {
-        return this.connect(this.path).get()
-            .then( e => {
-                let instruction = e.data().Instructions;
-                // console.log(instruction.language);
-                return instruction;
-            })
-    }
-
-    getUpdats() {
-        return this.connect(this.path).get()
-            .then( e => {
-                let updats = e.data().Updates;
-                // console.log(updats);
-                return updats;
-            })
-    }
-
-
-}
 
 class Verify {
     constructor(el, messageEl) {
